65b073a0317f2b1a8549399658458bd235c37351
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2020-10-08 16:40:58 +0300

    Build increased

--- Messagers/Discord/Commands/BasicCommands.cs
+++ Messagers/Discord/Commands/BasicCommands.cs
@@ -28,7 +28,7 @@ namespace BeatSlayerServer.Services.Messaging.Discord.Commands
                 Color = new DiscordColor(32, 32, 32)
             };
             builder.AddField("Environment", $"Service");
-            builder.AddField("Version", "1.1.1 (build 0)");
+            builder.AddField("Version", "1.1.1 (build 1)");
 
             await ctx.Channel.SendMessageAsync("Hello guys", embed: builder.Build()).ConfigureAwait(false);
         }
